399540d67e24c63dc607255e8b0be767a29dbe41
Kevin Jeffery <kevin@microknight.com>
2025-02-13 20:08:55 -0500

    Add javaversion module

---
+++ middleware/plugins/modules/javaversion.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# middleware/plugins/modules/javaversion.py
+# @version v1.00_2024-DEC-10
+# @author Kevin Jeffery
+
+from ansible.module_utils.basic import AnsibleModule # type: ignore[import]
+import os
+
+class javaversion:
+  def __init__(self, module):
+    self.module = module
+    self.module.debug("*** Process all Arguments")
+    self.java_home  = self.module.params['java_home']
+  
+  def version(self):
+    data = dict(version='none', installed=False)
+    if os.path.exists(self.java_home):
+      data['installed'] = True
+      stderr = self._run_command('-version')[2]
+      stderr_lines = stderr.split('\n')
+      for line in stderr_lines:
+        parts = line.split(' ')
+        if line.startswith('java version'):
+          data['version'] = parts[2].replace('"', '').replace('-', '.').replace('_','.')
+          continue
+        if line.startswith('Eclipse OpenJ9 VM'):
+          data['version'] = parts[3].replace('"', '').replace('-', '.').replace('_','.')
+          continue
+        if line.startswith('Java(TM)') or line.startswith('IBM Semeru'):
+          data['name'] = line
+          continue
+        if line.startswith('Java HotSpot'):
+          data['hotspot'] = line
+    self.module.exit_json(changed=False, data=data)
+  
+  def _run_command(self, params):
+    cmd = '{0}/bin/java {1}'.format(self.java_home, params)
+    rc, stdout, stderr = self.module.run_command(cmd, use_unsafe_shell=True)
+    if rc > 0:
+      self.module.fail_json(changed=False, rc=rc, stdout=stdout, stderr=stderr)
+    return rc, stdout, stderr
+
+def main():
+  module = AnsibleModule(
+    argument_spec=dict(
+      java_home=dict(required=True, type='str')
+    ),
+    supports_check_mode=False
+  )
+  inst = javaversion(module)
+  inst.version()
+
+if __name__ == '__main__':
+    main()
