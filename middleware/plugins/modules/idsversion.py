8bd99480ab1536b5facc5d31e261e5e6617d9094
Kevin Jeffery <kevin@microknight.com>
2025-03-19 22:22:21 -0400

    Add installed_versions

--- middleware/plugins/modules/idsversion.py
+++ middleware/plugins/modules/idsversion.py
@@ -27,7 +27,7 @@ class IDSVersion:
     return 'n.a.'
   
   def _get_version_info(self, compare_version=None):
-    data = dict(installed=False, version='0.0.0.0')
+    data = dict(installed=False, version='0.0.0.0', installed_versions=[])
     if os.path.exists(self.ldaphome) is False:
       return data
     data['installed'] = True
